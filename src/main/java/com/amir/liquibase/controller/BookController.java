d588690d16728b1e6044f64b708fa12880cf32ab
mdamiralamansari <amir.1997.ansari@gmail.com>
2023-04-08 02:04:38 +0530

    Service class name changed

--- src/main/java/com/amir/liquibase/controller/BookController.java
+++ src/main/java/com/amir/liquibase/controller/BookController.java
@@ -1,8 +1,7 @@
 package com.amir.liquibase.controller;
 
 import com.amir.liquibase.entity.Book;
-import com.amir.liquibase.repository.BookRepository;
-import com.amir.liquibase.service.BookService;
+import com.amir.liquibase.service.BookAndAuthorService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,12 +13,12 @@ import java.util.List;
 @RequestMapping("api/v1")
 @RequiredArgsConstructor
 public class BookController {
-    private final BookService bookService;
+    private final BookAndAuthorService bookAndAuthorService;
 
     @GetMapping("/books")
     public List<Book> getBooks(){
 
-        List<Book> allBooks = bookService.getAllBooks();
+        List<Book> allBooks = bookAndAuthorService.getAllBooks();
         return allBooks;
 
     }
